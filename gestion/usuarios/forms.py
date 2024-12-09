3abf30f669005434b5c1b23c0595c18100541951
saulandresv <saul.vega2@gmail.com>
2024-12-09 17:35:09 -0300

    crud funcionando correctamente

--- gestion/usuarios/forms.py
+++ gestion/usuarios/forms.py
@@ -7,5 +7,11 @@ class UsuarioForm(forms.ModelForm):
         model = Usuario
         fields = ['username', 'first_name', 'last_name', 'email', 'fecha_nacimiento', 'rut', 'rol']
         widgets = {
-            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}),
+            'username': forms.TextInput(attrs={'class': 'form-group-input'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-group-input'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-group-input'}),
+            'email': forms.EmailInput(attrs={'class': 'form-group-input'}),
+            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date', 'class': 'form-group-input'}),
+            'rut': forms.TextInput(attrs={'class': 'form-group-input'}),
+            'rol': forms.Select(attrs={'class': 'form-group-input'}),
         }
